220aaa75f9cc33699876cfe0012df0365c663479
Gabriel Felipe <gabrielsouzafelipe12@gmail.com>
2023-02-15 15:21:39 -0300

    formatar

--- src/operacoes.js
+++ src/operacoes.js
@@ -1,22 +1,21 @@
 function sum(num1, num2) {
-    if(isNaN(num1) || isNaN(num2)) {
-        throw "Não é um número"
-    }
-    return num1 + num2
+  if (isNaN(num1) || isNaN(num2)) {
+    throw "Não é um número";
+  }
+  return num1 + num2;
 }
 
 function sub(num1, num2) {
-    if(isNaN(num1) || isNaN(num2)) {
-        throw "Não é um número"
-    }
-    return num1 - num2
+  if (isNaN(num1) || isNaN(num2)) {
+    throw "Não é um número";
+  }
+  return num1 - num2;
 }
 
 module.exports = {
-    sum,
-    sub
-}
-
+  sum,
+  sub,
+};
 
 // Math.sqrt raiz
 // Math.pow potencia
--- test/operacao.test.js
+++ test/operacao.test.js
@@ -1,36 +1,32 @@
-const { describe, expect, it} = require('@jest/globals')
-const calculadora = require('../src/operacoes')
+const { describe, expect, it } = require("@jest/globals");
+const calculadora = require("../src/operacoes");
 
-describe('Teste de calculadora', () => {
+describe("Teste de calculadora", () => {
+  it("Operação de some", () => {
+    let resultado = calculadora.sum(1, 2);
+    expect(resultado).toEqual(3);
 
-    it('Operação de some', () => {
-        let resultado = calculadora.sum(1, 2);
-        expect(resultado).toEqual(3)
+    resultado = calculadora.sum(-1, 2);
+    expect(resultado).toEqual(1);
 
-        resultado = calculadora.sum(-1, 2);
-        expect(resultado).toEqual(1)
+    resultado = () => calculadora.sum("a", 2);
+    expect(resultado).toThrow("Não é um número");
 
-        resultado = () => calculadora.sum("a", 2);
-        expect(resultado).toThrow("Não é um número");
+    resultado = () => calculadora.sum(2, "b");
+    expect(resultado).toThrow("Não é um número");
+  });
 
-        resultado = () => calculadora.sum(2, "b");
-        expect(resultado).toThrow("Não é um número");
-    })
+  it("Operação de subtração", () => {
+    let resultado = calculadora.sub(1, 2);
+    expect(resultado).toEqual(-1);
 
+    resultado = calculadora.sub(4, 2);
+    expect(resultado).toEqual(2);
 
-    it('Operação de subtração', () => {
-        let resultado = calculadora.sub(1, 2);
-        expect(resultado).toEqual(-1)
+    resultado = () => calculadora.sub("a", -2);
+    expect(resultado).toThrow("Não é um número");
 
-        resultado = calculadora.sub(4, 2);
-        expect(resultado).toEqual(2)
-
-        resultado = () => calculadora.sub("a", -2);
-        expect(resultado).toThrow("Não é um número");
-
-        resultado = () => calculadora.sub(-2, "a");
-        expect(resultado).toThrow("Não é um número");
-    })
-
-})
-
+    resultado = () => calculadora.sub(-2, "a");
+    expect(resultado).toThrow("Não é um número");
+  });
+});
